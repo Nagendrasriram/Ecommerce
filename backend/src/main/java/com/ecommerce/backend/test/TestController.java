0f2aeb44f0b360ccc4c7816705aaf32ef3fe5609
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-06 20:56:28 +0530

    New_File_in_testController

--- backend/src/main/java/com/ecommerce/backend/test/TestController.java
+++ backend/src/main/java/com/ecommerce/backend/test/TestController.java
@@ -1,4 +1,13 @@
 package com.ecommerce.backend.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/api/secure")
+    public String secureEndpoint() {
+        return "JWT is working! You are authenticated 🎉";
+    }
 }
