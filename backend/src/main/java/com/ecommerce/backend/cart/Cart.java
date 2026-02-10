8f609616d208bd730e24e0ed9104a998de3f676b
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-10 06:14:26 +0530

    New_Package_Cart.java_File

--- backend/src/main/java/com/ecommerce/backend/cart/Cart.java
+++ backend/src/main/java/com/ecommerce/backend/cart/Cart.java
@@ -1,5 +1,37 @@
 package com.ecommerce.backend.cart;
-
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name ="cart")
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userid;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
