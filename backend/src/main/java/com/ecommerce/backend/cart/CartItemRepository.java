289fa9802ea0de4bc0e638301032ee9d7fee370c
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-10 06:20:11 +0530

    New_Package_CartItemRepository&Item.java_File

--- backend/src/main/java/com/ecommerce/backend/cart/CartItemRepository.java
+++ backend/src/main/java/com/ecommerce/backend/cart/CartItemRepository.java
@@ -1,7 +1,13 @@
 package com.ecommerce.backend.cart;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
-public interface CartItemRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findByUserId(Long userId);
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    List<CartItem> findByCartId(Long cartId);
+
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
