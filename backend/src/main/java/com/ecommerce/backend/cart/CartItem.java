464a334b4a8e318dc089d03756e5e42c9ffb428e
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-10 06:16:26 +0530

    New_Package_CartItem.java_File

--- backend/src/main/java/com/ecommerce/backend/cart/CartItem.java
+++ backend/src/main/java/com/ecommerce/backend/cart/CartItem.java
@@ -1,4 +1,45 @@
 package com.ecommerce.backend.cart;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "cart_item")
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long cartId;
+    private Long productId;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
