f85cf13bff5bdeab5b3f25323c01271d314b79d6
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-12 17:07:48 +0530

    New_Order_Pages1

---
+++ backend/src/main/java/com/ecommerce/backend/order/Order.java
@@ -0,0 +1,61 @@
+package com.ecommerce.backend.order;
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userid;
+
+    private Double totalAmount;
+
+    private String status = "PLACED";
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
