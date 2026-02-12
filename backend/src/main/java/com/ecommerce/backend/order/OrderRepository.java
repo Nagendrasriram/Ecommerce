63e142f61daa502784f8b5363525b043315198ea
Nagendrasriram <nagendra3feb@gmail.com>
2026-02-12 17:09:39 +0530

    New_Order_OrderRepository

---
+++ backend/src/main/java/com/ecommerce/backend/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.backend.order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserid(Long userid);
+}
