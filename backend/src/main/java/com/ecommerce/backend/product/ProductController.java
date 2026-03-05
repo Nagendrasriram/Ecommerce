133fc53bee1e137406b3b3ce203a1af9e5840e19
Nagendrasriram <nagendra3feb@gmail.com>
2026-03-05 22:38:44 +0530

    New_React_STarted

--- backend/src/main/java/com/ecommerce/backend/product/ProductController.java
+++ backend/src/main/java/com/ecommerce/backend/product/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/products")
 public class ProductController {
 
